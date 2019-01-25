1aa301bfaaf7d148b8f4cd5dfa9cf4a996454e66
baswa <baswa@xmdevelopments.com>
2019-01-25 14:05:22 +1300

    update env to match with example

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -1,6 +1,4 @@
-require('./src/config/envConfig');
-
-const seleniumServer = require('selenium-server');
+const seleniumServer = require('selenium-server-standalone-jar');
 const chromeDriver = require('chromedriver');
 const ieDriver = require('iedriver');
 const geckoDriver = require('geckodriver');
@@ -12,39 +10,25 @@ module.exports = {
   custom_assertions_path: '',
   stepDefinitions: 'src/frontend/stepDefinitions',
   page_objects_path: 'src/frontend/pageObjects',
-  live_output: false,
-  disable_colors: false,
-  test_workers: {
-    enabled: process.env.ParallelRun ? JSON.parse(process.env.ParallelRun) : false,
-    workers: 'auto'
-  },
   selenium: {
     start_process: true,
     server_path: seleniumServer.path,
-    log_path: 'reports/',
     host: '127.0.0.1',
     port: 4444,
     cli_args: {
       'webdriver.chrome.driver': chromeDriver.path,
       'webdriver.gecko.driver': geckoDriver.path,
       'webdriver.ie.driver': ieDriver.path,
-    }
+    },
   },
   test_settings: {
     default: {
-      screenshots: {
-        enabled: true,
-        on_error: true,
-        on_failure: true,
-        path: 'reports/frontEnd/screenShots',
-      },
       desiredCapabilities: {
         browserName: 'chrome',
         javascriptEnabled: true,
         acceptSslCerts: true,
-        elementScrollBehavior: 1,
         chromeOptions: {
-          args: ['--headless', '--disable-gpu']
+          args: ['--headless', '--disable-gpu'],
         },
       },
     },
@@ -53,14 +37,8 @@ module.exports = {
         browserName: 'chrome',
         javascriptEnabled: true,
         acceptSslCerts: true,
-        elementScrollBehavior: 1,
         chromeOptions: {
           args: ['--window-size=1920,1080'],
-        },
-      },
-      selenium: {
-        cli_args: {
-          'webdriver.chrome.driver': chromeDriver.path,
         },
       },
     },
@@ -72,25 +50,14 @@ module.exports = {
         elementScrollBehavior: 1,
         ieOptions: {
           args: ['--window-size=1920,1080'],
-        }
-      },
-      selenium: {
-        cli_args: {
-          'webdriver.ie.driver': ieDriver.path,
         },
       },
     },
-
     firefox: {
       desiredCapabilities: {
         browserName: 'firefox',
         javascriptEnabled: true,
         acceptSslCerts: true,
-      },
-      selenium: {
-        cli_args: {
-          'webdriver.gecko.driver': geckoDriver.path,
-        },
       },
     },
   },
--- src/helper/constants/constants.js
+++ src/helper/constants/constants.js
@@ -1,20 +1,20 @@
-const environmentVariable = {
-  frontEndBaseURL:
-    process.env.envname === 'int'
-      ? 'https://sage300v2017-int.dev-mysagestore.com/user/logout'
-      : 'https://sage300v2017-int.dev-mysagestore.com/user/logout',
-  baseBackendURL:
-    process.env.envname === 'staging'
-      ? 'https://sage300v2017.staging.mysagestore.com/user/logout'
-      : ''
-}
+// const environmentVariable = {
+//   frontEndBaseURL:
+//     process.env.envname === 'int'
+//       ? 'https://sage300v2017-int.dev-mysagestore.com/user/logout'
+//       : 'https://sage300v2017-int.dev-mysagestore.com/user/logout',
+//   baseBackendURL:
+//     process.env.envname === 'staging'
+//       ? 'https://sage300v2017.staging.mysagestore.com/user/logout'
+//       : '',
+// };
 
 const loginCredentials = {
   automationTestUser: 'test',
-  automationPassword: 'test'
-}
+  automationPassword: 'test',
+};
 
 module.exports = {
   loginCredentials,
-  waitForConditionTimeout: 60000
-}
+  waitForConditionTimeout: 60000,
+};
